2f1230a822446336b236d21702a54b81fa30b54a
fionarhuang <fionarhuang@gmail.com>
2018-09-06 10:09:38 +0200

    add R codes to export SingleCellExperiment and SummarizedExperiment

--- scripts/prepare_results_for_shiny.R
+++ scripts/prepare_results_for_shiny.R
@@ -31,30 +31,30 @@ genes <- tx2gene %>% dplyr::select(-tx, -tx_biotype, -start, -end) %>% dplyr::di
 ## edgeR result table with all contrasts ("wide")
 edgeRwide <- edgerres$results
 if (class(edgeRwide) == "list" && class(edgeRwide) != "data.frame") {
-  for (i in seq_len(length(edgeRwide))) {
-    idx <- which(colnames(edgeRwide[[i]]) %in% c("logFC", "F", "PValue", "FDR", "LR"))
-    colnames(edgeRwide[[i]])[idx] <- paste0(colnames(edgeRwide[[i]])[idx], ".", names(edgeRwide)[i])
-  }
-  edgeRwide <- Reduce(function(...) dplyr::full_join(...), edgeRwide)
+    for (i in seq_len(length(edgeRwide))) {
+        idx <- which(colnames(edgeRwide[[i]]) %in% c("logFC", "F", "PValue", "FDR", "LR"))
+        colnames(edgeRwide[[i]])[idx] <- paste0(colnames(edgeRwide[[i]])[idx], ".", names(edgeRwide)[i])
+    }
+    edgeRwide <- Reduce(function(...) dplyr::full_join(...), edgeRwide)
 }
 
 edgeRwide <- edgeRwide %>% 
-  dplyr::select(gene, symbol, gene_biotype, logCPM, everything()) %>%
-  dplyr::mutate(gene_biotype = factor(gene_biotype)) %>%
-  dplyr::mutate(strand = factor(strand))
+    dplyr::select(gene, symbol, gene_biotype, logCPM, everything()) %>%
+    dplyr::mutate(gene_biotype = factor(gene_biotype)) %>%
+    dplyr::mutate(strand = factor(strand))
 
 ## edgeR result table for volcano plots ("long")
 edgeRlong <- edgeRwide %>% 
-  tidyr::gather(typecontrast, value, -gene, -symbol, -gene_biotype, -logCPM, 
-                -chromosome, -strand) %>%
-  dplyr::mutate(typecontrast = gsub("logFC\\.", "logFC_", typecontrast)) %>%
-  dplyr::mutate(typecontrast = gsub("FDR\\.", "FDR_", typecontrast)) %>%
-  dplyr::mutate(typecontrast = gsub("PValue\\.", "PValue_", typecontrast)) %>%
-  dplyr::mutate(typecontrast = gsub("F\\.", "F_", typecontrast)) %>%
-  dplyr::mutate(typecontrast = gsub("LR\\.", "LR_", typecontrast)) %>%
-  tidyr::separate(typecontrast, into = c("dtype", "contrast"), sep = "_") %>%
-  tidyr::spread(key = dtype, value = value) %>%
-  dplyr::mutate(mlog10PValue = -log10(PValue))
+    tidyr::gather(typecontrast, value, -gene, -symbol, -gene_biotype, -logCPM, 
+                  -chromosome, -strand) %>%
+    dplyr::mutate(typecontrast = gsub("logFC\\.", "logFC_", typecontrast)) %>%
+    dplyr::mutate(typecontrast = gsub("FDR\\.", "FDR_", typecontrast)) %>%
+    dplyr::mutate(typecontrast = gsub("PValue\\.", "PValue_", typecontrast)) %>%
+    dplyr::mutate(typecontrast = gsub("F\\.", "F_", typecontrast)) %>%
+    dplyr::mutate(typecontrast = gsub("LR\\.", "LR_", typecontrast)) %>%
+    tidyr::separate(typecontrast, into = c("dtype", "contrast"), sep = "_") %>%
+    tidyr::spread(key = dtype, value = value) %>%
+    dplyr::mutate(mlog10PValue = -log10(PValue))
 
 ## -------------------------------------------------------------------------- ##
 ##                             Gene models                                    ##
@@ -109,6 +109,95 @@ mds <- as.data.frame(mds) %>% tibble::rownames_to_column(var = "ID") %>%
 logcpms <- reshape2::melt(as.matrix(logcpms)) %>%
   dplyr::rename(gene = Var1, sample = Var2) %>%
   dplyr::mutate(group = metadata[match(sample, metadata$ID), groupvar])
+
+## -------------------------------------------------------------------------- ##
+##                             export - SCE  & SE                             ##
+## -------------------------------------------------------------------------- ##
+library(SingleCellExperiment)
+library(S4Vectors)
+## row data
+# information about rows
+rData <- tx2gene %>%
+    dplyr::select(-tx, -tx_biotype, -start, -end) %>% 
+    dplyr::distinct() %>% 
+    dplyr::filter(gene %in% rownames(dge)) %>%
+    dplyr::arrange(match(gene, rownames(dge)))
+rData <- S4Vectors::DataFrame(rData)
+
+# output from edgeR
+typeContrast <- unique(edgeRlong$contrast)
+resList <- lapply(seq_along(typeContrast), FUN = function(x) {
+    # rows kept in the analysis
+    data.x <- edgeRlong %>% 
+        dplyr::filter(contrast == typeContrast[x]) %>%
+        dplyr::select(-symbol, -gene_biotype, -logCPM, 
+                      -chromosome, -strand, -contrast)
+    # add rows for those filtered out
+    geneOUT <- setdiff(rownames(dge), data.x$gene)
+    if (length(geneOUT) > 0) {
+        data.x <- data.frame(matrix(NA, nrow = length(geneOUT), 
+                                    ncol = ncol(data.x), 
+                                    dimnames = list(geneOUT, colnames(data.x))
+        )) %>%
+            dplyr::mutate(gene = geneOUT) %>%
+            dplyr::bind_rows(data.x) %>%
+            dplyr::arrange(match(gene, rownames(dge)))
+        
+    } else {
+        data.x <- data.x
+    }
+    
+    # use gene column as rownames
+    rownames(data.x) <- data.x$gene
+    data.f <- data.x %>%
+        dplyr::select(-gene)
+    data.f <- S4Vectors::DataFrame(data.f)
+    
+    return(data.f)
+    
+})
+names(resList) <- typeContrast
+
+# put edgeR output in the rowData
+for (i in seq_along(resList)) {
+    tc.i <- typeContrast[i]
+    rData[[tc.i]] <- resList[[i]] 
+}
+
+
+## column data
+cData1 <- data.frame(ID = colnames(dge), 
+                     bwFiles = bwfiles[colnames(dge)], 
+                     bwCond = condition[colnames(dge)]) %>%
+    dplyr::arrange(match(ID, colnames(dge)))
+cData2 <- mds %>% 
+    dplyr::arrange(match(ID, colnames(dge))) %>%
+    dplyr::select(type, group) 
+cData <- dplyr::bind_cols(cData1, cData2)
+cData <- DataFrame(cData)
+
+## assays data
+logCPM_count <- edgeR::cpm(dge, log = TRUE, prior.count = 2)
+aData <- list(rawCount = dge, logCPM = logCPM_count[rownames(dge), ])
+
+## low dimensional representations
+reduceData <- mds %>% 
+    dplyr::arrange(match(ID, colnames(dge))) %>%
+    dplyr::select(-ID, -type, -group) 
+reduceData <- as.matrix(reduceData)
+
+
+sce <- SingleCellExperiment(assays = aData, rowData = rData,
+                            colData = cData, 
+                            metadata = list(geneModels = genemodels,
+                                            geneInfo = genes),
+                            reducedDims = SimpleList(dimReds = reduceData))
+
+
+se <- SummarizedExperiment(assays = aData, rowData = rData,
+                           colData = cData, 
+                           metadata = list(geneModels = genemodels,
+                                           geneInfo = genes))
 
 ## -------------------------------------------------------------------------- ##
 ##                                 Save                                       ##
