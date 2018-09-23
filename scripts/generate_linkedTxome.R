784be735eee15aef04ffaa4974af4801a5c04220
fionarhuang <fionarhuang@gmail.com>
2018-09-23 12:09:27 +0200

    update the work on creating linkedTxome from other branch

---
+++ scripts/generate_linkedTxome.R
@@ -0,0 +1,31 @@
+args <- (commandArgs(trailingOnly = TRUE))
+for (i in 1:length(args)) {
+  eval(parse(text = args[[i]]))
+}
+
+suppressPackageStartupMessages(library(tximeta)) 
+
+print(transcriptfasta)
+print(salmonidx)
+print(gtf)
+
+print(annotation)
+ss <- strsplit(organism, "_")[[1]]
+organism <- paste(paste(ss[1], ss[2]))
+print(organism)
+print(release)
+print(build)
+print(output)
+
+makeLinkedTxome(indexDir=dirname(salmonidx),
+                source=annotation,
+                organism=organism,
+                release=release,
+                genome=build,
+                fasta=transcriptfasta,
+                gtf=gtf,
+                write=TRUE, 
+                jsonFile = output)
+
+sessionInfo()
+date()
