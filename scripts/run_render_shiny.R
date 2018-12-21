f8508b898e7f959139056219756317d8bc0814db
fionarhuang <fionarhuang@gmail.com>
2018-12-21 18:12:21 +0100

    - add .Rmd file for rule Shiny - add R script to render Rmd file for rule Shiny

---
+++ scripts/run_render_shiny.R
@@ -0,0 +1,20 @@
+args <- (commandArgs(trailingOnly = TRUE))
+for (i in 1:length(args)) {
+    eval(parse(text = args[[i]]))
+}
+
+print(se)
+print(edgerres)
+print(gtffile)
+print(rmdtemplate)
+print(bigwigdir)
+print(groupvar)
+print(outputdir)
+print(outputfile)
+
+source("scripts/generate_report.R")
+
+generateReport(se = se, rmdTemplate = rmdtemplate, outputDir = outputdir, 
+               outputFile = outputfile, forceOverwrite = TRUE,
+               showCode = TRUE, gtffile = gtffile, edgerres = edgerres,
+               bigwigdir = bigwigdir, groupvar = groupvar)
