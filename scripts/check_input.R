7cf60995930b54129638287d79c499582b3056d5
fionarhuang <fionarhuang@gmail.com>
2019-03-12 10:57:20 +0100

    update the R script to check inputs

--- scripts/check_input.R
+++ scripts/check_input.R
@@ -10,8 +10,34 @@ suppressPackageStartupMessages({
 ## ----------------The input arguments------------------------------------------
 print(metafile)
 print(outFile)
+print(gtf)
+print(genonme)
+print(fastqdir)
+print(fqsuffix)
 
 msg0 <- try({
+    pt <- paste0("\\.", fqsuffix, ".1gz")
+    lf <- list.files(pattern = pt, path = fastqdir)
+    if (length(lf) == 0) {
+        stop("FASTQ files with ", fqsuffix, " .gz are not available. \n")
+    }
+}, silent = TRUE)
+
+msg1 <- try({
+    fe <- file.exists(genonme)
+    if (!fe) {
+        stop("The genonme file doesn't exist. \n")
+    }
+}, silent = TRUE)
+
+msg2 <- try({
+    fe <- file.exists(gtf)
+    if (!fe) {
+        stop("The gtf file doesn't exist. \n")
+    }
+}, silent = TRUE)
+
+msg3 <- try({
     if (exists("design")) {
         print(design)
     } else {
@@ -20,7 +46,7 @@ msg0 <- try({
 }, silent = TRUE)
 
 
-msg1 <- try({
+msg4 <- try({
     if (exists("contrast")) {
         contrast <- strsplit(gsub(" ","",contrast), ",")[[1]]
         print(contrast)
@@ -36,15 +62,15 @@ rownames(metadata) <- metadata$names
 metadata
 
 ## Define design matrix
-msg2 <- try({des <- model.matrix(as.formula(design), data = metadata)},
+msg5 <- try({des <- model.matrix(as.formula(design), data = metadata)},
             silent = TRUE)
 
 # Define contrasts
-msg3 <- try({contrasts <- as.data.frame(makeContrasts(contrasts = contrast, 
+msg6 <- try({contrasts <- as.data.frame(makeContrasts(contrasts = contrast, 
                                                       levels = des))},
             silent = TRUE)
 
-msgL <- list(msg0, msg1, msg2, msg3)
+msgL <- list(msg0, msg1, msg2, msg3, msg4, msg5, msg6)
 isError <- lapply(msgL, FUN = function(x) {class(x) == "try-error"})
 isError <- unlist(isError)
 
