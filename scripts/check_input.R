a9a10749ae6356545039d23af7b2c5cd44b32c3f
fionarhuang <fionarhuang@gmail.com>
2019-03-12 12:26:18 +0100

    add tests to PE and SE fastq files

--- scripts/check_input.R
+++ scripts/check_input.R
@@ -9,38 +9,119 @@ suppressPackageStartupMessages({
 })
 
 ## ----------------The input arguments------------------------------------------
-print(metafile)
-print(outFile)
-print(gtf)
-print(genome)
-print(fastqdir)
-print(fqsuffix)
-print(txome)
-print(run_camera)
+if (exists("metafile")) {
+    print(metafile) 
+    } else {
+        metafile <- NULL
+    }
 
+if (exists("outFile")) {
+    print(outFile) 
+    } else {
+        outFile <- NULL
+    }
+
+if (exists("gtf")) {
+    print(gtf) 
+} else {
+    gtf <- NULL
+}
+
+if (exists("gtf")) {
+    print(gtf) 
+} else {
+    gtf <- NULL
+}
+
+if (exists("genome")) {
+    print(genome) 
+} else {
+    genome <- NULL
+}
+
+if (exists("fastqdir")) {
+    print(fastqdir) 
+} else {
+    fastqdir <- NULL
+}
+
+if (exists("fqsuffix")) {
+    print(fqsuffix) 
+} else {
+    fqsuffix <- NULL
+}
+
+if (exists("fqext1")) {
+    print(fqext1) 
+} else {
+    fqext1 <- NULL
+}
+
+if (exists("fqext2")) {
+    print(fqext2) 
+} else {
+    fqext2 <- NULL
+}
+if (exists("txome")) {
+    print(txome) 
+} else {
+    txome <- NULL
+}
+
+if (exists("run_camera")) {
+    print(run_camera) 
+} else {
+    run_camera <- NULL
+}
+
+
+## Read metadata
 msg0 <- try({
-    pt <- paste0("\\.", fqsuffix, ".1gz")
-    lf <- list.files(pattern = pt, path = fastqdir)
-    if (length(lf) == 0) {
-        stop("FASTQ files with ", fqsuffix, " .gz are not available. \n")
+    metadata <- read.delim(metafile, header = TRUE, 
+                           as.is = TRUE, sep = "\t");
+    rownames(metadata) <- metadata$names;
+    type <- metadata$type;
+    if (length(unique(type)) == 1 | any(type %in% c("PE", "SE"))) {
+        error("The type column in the metadata should have either PE or SE. \n")
+    }
+    }, 
+    silent = TRUE)
+
+
+msg1 <- try({
+    if (unique(type) == "SE") {
+        pt <- paste0(metadata$names, ".", fqsuffix, ".gz")
+        
+    } else {
+        pt1 <- paste0(metadata$names, "_", fqext1, ".", fqsuffix, ".gz")
+        pt2 <- paste0(metadata$names, "_", fqext2, ".", fqsuffix, ".gz")
+        pt <- c(pt1, pt2)
+    }
+    
+    lf <- file.path(fastqdir, pt)
+    fe <- file.exists(lf)
+    if (any(!fe)) {
+        
+        stop(fe[!fe], " are/is not available. \n")
     }
 }, silent = TRUE)
 
-msg1 <- try({
+msg2 <- try({
+    
     fe <- file.exists(genome)
     if (!fe) {
         stop("The genonme file doesn't exist. \n")
     }
 }, silent = TRUE)
 
-msg2 <- try({
+msg3 <- try({
     fe <- file.exists(gtf)
     if (!fe) {
         stop("The gtf file doesn't exist. \n")
     }
 }, silent = TRUE)
 
-msg3 <- trytry({
+msg4 <- try({
     fe <- file.exists(txome)
     if (!fe) {
         stop("The txome file doesn't exist. \n")
@@ -54,7 +135,7 @@ msg3 <- trytry({
 #     }
 # }, silent = TRUE)
 
-msg5 <- try({
+msg6 <- try({
     if (exists("design")) {
         print(design)
     } else {
@@ -63,7 +144,7 @@ msg5 <- try({
 }, silent = TRUE)
 
 
-msg6 <- try({
+msg7 <- try({
     if (exists("contrast")) {
         contrast <- strsplit(gsub(" ","",contrast), ",")[[1]]
         print(contrast)
@@ -73,21 +154,18 @@ msg6 <- try({
 }, silent = TRUE)
 
 ## ---------------------------Test run -------------------------------
-## Read metadata
-metadata <- read.delim(metafile, header = TRUE, as.is = TRUE, sep = "\t")
-rownames(metadata) <- metadata$names
-metadata
+
 
 ## Define design matrix
-msg7 <- try({des <- model.matrix(as.formula(design), data = metadata)},
+msg8 <- try({des <- model.matrix(as.formula(design), data = metadata)},
             silent = TRUE)
 
 # Define contrasts
-msg8 <- try({contrasts <- as.data.frame(makeContrasts(contrasts = contrast, 
+msg9 <- try({contrasts <- as.data.frame(makeContrasts(contrasts = contrast, 
                                                       levels = des))},
             silent = TRUE)
 
-msgL <- list(msg0, msg1, msg2, msg3, msg4, msg5, msg6, msg7, msg8)
+msgL <- list(msg0, msg1, msg2, msg3, msg4, msg5, msg6, msg7, msg8, msg9)
 isError <- lapply(msgL, FUN = function(x) {class(x) == "try-error"})
 isError <- unlist(isError)
 
